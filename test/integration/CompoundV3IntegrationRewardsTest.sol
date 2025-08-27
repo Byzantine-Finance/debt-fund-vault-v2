13b0a3d9b6f16708bd6de25ce0b7b54504923875
lin <lin-h@hotmail.fr>
2025-08-27 18:10:55 +0200

    test: Test CompoundV3Adapter claim and swap
    (used transaction data from LiFi getQuote)

--- test/integration/CompoundV3IntegrationRewardsTest.sol
+++ test/integration/CompoundV3IntegrationRewardsTest.sol
@@ -2,10 +2,20 @@
 pragma solidity ^0.8.0;
 
 import "./CompoundV3IntegrationTest.sol";
+import {stdJson} from "../../lib/forge-std/src/StdJson.sol";
 
 contract CompoundV3IntegrationRewardsTest is CompoundV3IntegrationTest {
     address immutable claimer = makeAddr("claimer");
     uint256 internal initialDeposit = 1e12;
+
+    // Load quote data from JSON file
+    string internal root = vm.projectRoot();
+    string internal path = string.concat(root, "/test/data/quote_data_from_swapper.json");
+
+    // The transaction data from the quote
+    bytes internal transactionData;
+    uint256 internal minAmountOut; // only used to assert
+    address internal vaultUsedInQuote;
 
     function setUp() public virtual override {
         super.setUp();
@@ -26,26 +36,52 @@ contract CompoundV3IntegrationRewardsTest is CompoundV3IntegrationTest {
     }
 
     /// forge-config: default.isolate = true
-    function testClaimRewards(uint256 elapsed) public {
-        elapsed = bound(elapsed, 0, 365 days);
+    function testClaimRewards() public {
+        _loadQuoteData(path);
 
-        CometRewardsInterface.RewardOwed memory rewardOwed =
-            cometRewards.getRewardOwed(address(comet), address(compoundAdapter));
-        address rewardToken = rewardOwed.token;
-        uint256 rewardsBefore = rewardOwed.owed;
+        // etch the vault address used in the quote
+        vm.etch(vaultUsedInQuote, address(vault).code);
 
-        skip(elapsed);
+        // get the reward token
+        address rewardToken = cometRewards.getRewardOwed(address(comet), address(compoundAdapter)).token;
 
-        uint256 rewardsAfter = cometRewards.getRewardOwed(address(comet), address(compoundAdapter)).owed;
+        skip(50 days);
+
+        // get the accumulated rewards
+        uint256 owed = cometRewards.getRewardOwed(address(comet), address(compoundAdapter)).owed;
+
+        // vault's USDC balance before the claim
+        uint256 vaultUSDCBalanceBefore = IERC20(usdc).balanceOf(vaultUsedInQuote);
+
+        // encode the data to claim the rewards
+        bytes memory data = abi.encode(transactionData, LIFI_DIAMOND);
+
+        vm.startPrank(claimer);
 
         vm.expectEmit();
-        emit ICompoundV3Adapter.Claim(rewardToken, rewardsAfter - rewardsBefore);
+        emit ICompoundV3Adapter.Claim(rewardToken, owed);
 
-        vm.prank(claimer);
-        compoundAdapter.claim();
+        compoundAdapter.claim(data);
+        vm.stopPrank();
 
-        assertEq(cometRewards.rewardsClaimed(address(comet), address(compoundAdapter)), rewardsAfter - rewardsBefore);
-        assertEq(IERC20(rewardToken).balanceOf(address(compoundAdapter)), rewardsAfter - rewardsBefore);
+        // Verify the vault's USDC balance has the minimum amount out provided in the quote
+        assertGe(IERC20(usdc).balanceOf(vaultUsedInQuote) - vaultUSDCBalanceBefore, minAmountOut);
+
+        assertEq(cometRewards.rewardsClaimed(address(comet), address(compoundAdapter)), owed);
+        assertEq(IERC20(rewardToken).balanceOf(address(compoundAdapter)), 0);
         assertEq(cometRewards.getRewardOwed(address(comet), address(compoundAdapter)).owed, 0);
+    }
+
+    function _loadQuoteData(string memory _path) internal {
+        string memory json = vm.readFile(_path);
+
+        // Parse transactionData
+        transactionData = stdJson.readBytes(json, ".transactionData");
+
+        // Parse minAmountOut
+        minAmountOut = stdJson.readUint(json, ".minAmountOut");
+
+        // Parse vaultUsedInQuote
+        vaultUsedInQuote = stdJson.readAddress(json, ".vaultUsedInQuote");
     }
 }
