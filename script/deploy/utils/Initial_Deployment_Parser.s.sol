84ee06305a39c4ec6fa1a491a81af610f687b1db
lin <lin-h@hotmail.fr>
2025-10-10 13:06:34 +0200

    chore: Add mainnet deployment script for Adapter Factories

--- script/deploy/utils/Initial_Deployment_Parser.s.sol
+++ script/deploy/utils/Initial_Deployment_Parser.s.sol
@@ -12,17 +12,17 @@ import "forge-std/Test.sol";
 
 contract Initial_Deployment_Parser is Script, Test {
     // Factories
-    VaultV2Factory public vaultV2Factory;
-    MorphoMarketV1AdapterFactory public morphoMarketV1AdapterFactory;
-    MorphoVaultV1AdapterFactory public erc4626AdapterFactory;
+    // VaultV2Factory public vaultV2Factory;
+    // MorphoMarketV1AdapterFactory public morphoMarketV1AdapterFactory;
+    // MorphoVaultV1AdapterFactory public erc4626AdapterFactory;
     ERC4626MerklAdapterFactory public erc4626MerklAdapterFactory;
     CompoundV3AdapterFactory public compoundV3AdapterFactory;
 
     function _deployFromScratch() internal {
         // Deploy Factories
-        vaultV2Factory = new VaultV2Factory();
-        morphoMarketV1AdapterFactory = new MorphoMarketV1AdapterFactory();
-        erc4626AdapterFactory = new MorphoVaultV1AdapterFactory();
+        // vaultV2Factory = new VaultV2Factory();
+        // morphoMarketV1AdapterFactory = new MorphoMarketV1AdapterFactory();
+        // erc4626AdapterFactory = new MorphoVaultV1AdapterFactory();
         erc4626MerklAdapterFactory = new ERC4626MerklAdapterFactory();
         compoundV3AdapterFactory = new CompoundV3AdapterFactory();
     }
@@ -33,10 +33,10 @@ contract Initial_Deployment_Parser is Script, Test {
         string memory deployed_addresses = "addresses";
         string memory chain_info = "chainInfo";
 
-        vm.serializeAddress(deployed_addresses, "vaultV2Factory", address(vaultV2Factory));
+        // vm.serializeAddress(deployed_addresses, "vaultV2Factory", address(vaultV2Factory));
         vm.serializeAddress(deployed_addresses, "compoundV3AdapterFactory", address(compoundV3AdapterFactory));
-        vm.serializeAddress(deployed_addresses, "morphoMarketV1AdapterFactory", address(morphoMarketV1AdapterFactory));
-        vm.serializeAddress(deployed_addresses, "erc4626AdapterFactory", address(erc4626AdapterFactory));
+        // vm.serializeAddress(deployed_addresses, "morphoMarketV1AdapterFactory", address(morphoMarketV1AdapterFactory));
+        // vm.serializeAddress(deployed_addresses, "erc4626AdapterFactory", address(erc4626AdapterFactory));
         string memory deployed_addresses_output =
             vm.serializeAddress(deployed_addresses, "erc4626MerklAdapterFactory", address(erc4626MerklAdapterFactory));
 
